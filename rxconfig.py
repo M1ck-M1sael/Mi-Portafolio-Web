a966b62466d2b61a6f049fcbb35be25cbdbed28f
M1ck-M1sael <mickmisa3l@gmail.com>
2026-04-30 22:12:36 -0600

    configuración final para despliegue en AWS oficial, ya está en la nuve y disponible a internet

--- rxconfig.py
+++ rxconfig.py
@@ -2,7 +2,7 @@ import reflex as rx
 
 config = rx.Config(
     app_name="Portafolio_Web_Mick",
-    api_url="http://54.145.241.44:8000", 
+    api_url="http://13.221.97.68:8000", 
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
